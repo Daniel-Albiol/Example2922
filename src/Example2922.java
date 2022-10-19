1d368a6d6dda1306d6a4c234df2add95ab5598c1
datst <alualb932@ieselcaminas.org>
2022-10-19 23:17:59 +0200

    Initial commit

---
+++ src/Example2922.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Example2922 {
+    public static void main(String[] argv) {
+        int dividend, divisor;
+        System.out.println("Enter dividend:");
+//Reading the value
+        Scanner inputValue;
+        inputValue = new Scanner(System.in);
+        dividend = inputValue.nextInt();
+        System.out.println("Enter divisor:");
+        divisor = inputValue.nextInt();
+        while (dividend >= divisor) {
+            dividend = dividend - divisor;
+        }
+        System.out.println(dividend);
+    }
+}
